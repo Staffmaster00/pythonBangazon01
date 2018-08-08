b73536cd55a87b86f96ba545f697542cb47f718e
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2018-08-08 15:01:16 -0500

    Clarity Revision

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # pythonBangazon01
+
+## Instructions:
+
+     Simply run python humansizes.py on the provided python file to see how python uses objects and def
+
+## What I learned:
+
+     Everything in Python is an object like Javascript without primative forms.
+     The entire module is run as this object in the CLI and given properties called _name_ with
+     a value of _main_
+
+     print() = console.log()
+     objects are called dictionaries in Python
+     whitespace matters
+     if and for use colons instead with indentation after
+     functions work simarily but are called defintions? they use def instead of function.
+     compliler determines values at runtime
--- humansizes.py
+++ humansizes.py
@@ -26,18 +26,3 @@ if __name__ == '__main__':
     print(approximate_size(16384, False))
     print(approximate_size(size=16384, a_kilobyte_is_1024_bytes=True))
     print(approximate_size(-16384))
-
-    # this is an example provided by NSS/bangazon of the style for a ternary condition in Python
-    # since they didn't have me do much besides run the code using python humansizes.py 
-    # I'll put what I learned here instead...
-
-    # Everything in Python is an object like Javascript without primative forms.
-    # The entire module is run as this object in the CLI and given properties called _name_ with
-    # a value of _main_
-
-    # print() = console.log()
-    # objects are called dictionaries in Python
-    # whitespace matters
-    # if and for use colons instead with indentation after
-    # functions work simarily but are called defintions? they use def instead of function.
-    # compliler determines values at runtime
